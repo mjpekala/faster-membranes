fc808f8fe54a8ed39cca98bf748de3445a2bcc7f
mike pekala <mpekala@umd.edu>
2015-10-23 14:23:18 -0400

    now also thresholds labels

--- Data/Kasthuri11/getdata.sh
+++ Data/Kasthuri11/getdata.sh
@@ -2,22 +2,27 @@
 
 
 echo ""
-echo "Obtaining EM training data - please wait a few moments..."
+echo "[info]: obtaining EM training data - please wait a few moments..."
 echo ""
 curl http://openconnecto.me/ocp/ca/kasthuri11cc/image/hdf5/1/4400,5424/5440,6464/1100,1200/ > train-volume.hdf5
+echo "[info]: extracting volume..."
 python extract_volume.py train-volume.hdf5
 
 
 echo ""
-echo "Obtaining training data labels - please wait a few moments..."
+echo "[info]: obtaining training data labels - please wait a few moments..."
 echo ""
 curl http://openconnecto.me/ocp/ca/ac3ac4/ac4membraneIDSIA/hdf5/1/4400,5424/5440,6464/1100,1200/ > train-labels.hdf5
+echo "[info]: extracting volume..."
 python extract_volume.py train-labels.hdf5
+echo "[info]: thresholding probabilities..."
+python threshold_labels.py train-labels.npy
 
 
 echo ""
-echo "Obtaining EM test data - please wait a few moments..."
+echo "[info]: obtaining EM test data - please wait a few moments..."
 echo ""
 curl http://openconnecto.me/ocp/ca/kasthuri11cc/image/hdf5/1/1/5472,6496/8712,9736/1000,1256/ > test-volume.hdf5
+echo "[info]: extracting volume..."
 python extract_volume.py test-volume.hdf5
 
